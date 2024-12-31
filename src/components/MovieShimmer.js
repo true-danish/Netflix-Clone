0a083b041629bf92b6d5daab5b2ab9526b177ca0
danish <sendmailtodanish@gmail.com>
2024-12-31 14:32:00 +0530

    Revert "initial"
    This reverts commit 753df8a3a3bad5f3c3545bf8acc1fa22bc6c62e5.

--- src/components/MovieShimmer.js
+++
@@ -1,7 +0,0 @@
-const MovieShimmer = () => {
-  return (
-    <div className="w-44 max-sm:w-28 h-80 bg-gray-200  aspect-[2/3]  shrink-0  hover:scale-95 hover:outline-1 outline outline-white outline-0  transition-all"></div>
-  );
-};
-
-export default MovieShimmer;
